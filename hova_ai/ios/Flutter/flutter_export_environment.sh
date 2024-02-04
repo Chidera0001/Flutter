e96866fb5bb9db7863e29159386963d19d3f4149
Chidera0001 <c.anele@alustudent.com>
2024-02-04 11:09:24 +0200

    task

--- hova_ai/ios/Flutter/flutter_export_environment.sh
+++ hova_ai/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\Users\ADMIN\AppData\Local\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\ADMIN\OneDrive\Desktop\hova_ai"
+export "FLUTTER_APPLICATION_PATH=C:\Users\ADMIN\OneDrive\Desktop\Flutter\hova_ai"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
--- hova_ai/macos/Flutter/ephemeral/flutter_export_environment.sh
+++ hova_ai/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\Users\ADMIN\AppData\Local\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\ADMIN\OneDrive\Desktop\hova_ai"
+export "FLUTTER_APPLICATION_PATH=C:\Users\ADMIN\OneDrive\Desktop\Flutter\hova_ai"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
